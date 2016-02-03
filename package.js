0ec96eb742a78ca8f480ccda8bf4b1317ae32cfc
Frederic G. MARAND <fgm@osinet.fr>
2016-02-03 10:24:51 +0100

    Updated package.js to add explicit dependencies on check, http, random.
    - These packages used to be implicit when importing meteor, but no longer are.

--- package.js
+++ package.js
@@ -1,9 +1,17 @@
 Package.describe({
-  summary: "DB less realtime communication for meteor"
+  name: "fgm:streams",
+  version: "0.1.18",
+  git: "https://github.com/FGM/meteor-streams",
+  documentation: "README.md",
+  summary: "DB less realtime communication for meteor. A fork of arunoda:streams for Meteor 1.2.1."
 });
 
 Package.on_use(function (api, where) {
+  api.use('http');
+  api.use('check');
+  api.use('random');
   api.use('underscore', ['client', 'server']);
-  api.add_files(['lib/ev.js', 'lib/server.js', 'lib/stream_permission.js'], 'server');
-  api.add_files(['lib/ev.js', 'lib/client.js'], 'client');
+
+  api.addFiles(['lib/ev.js', 'lib/server.js', 'lib/stream_permission.js'], 'server');
+  api.addFiles(['lib/ev.js', 'lib/client.js'], 'client');
 });
